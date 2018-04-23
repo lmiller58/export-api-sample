0705400d6afeddb475541c82a58e68711974eae5
a-yu-relativity <ayu@kcura.com>
2018-04-23 16:04:00 -0500

    Half implement document export

--- ExportApiSample/Documents.cs
+++ ExportApiSample/Documents.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Relativity.Kepler.Transport;
+using Relativity.Services.DataContracts.ImportExport;
 using Relativity.Services.Objects;
 using Relativity.Services.Objects.DataContracts;
 
@@ -16,12 +17,12 @@ namespace ExportApiSample
 
 
         /// <summary>
-        /// Returns the number of digits in an integer. 
+        /// Returns the number of digits in an integer (base 10). 
         /// If negative, the negative sign does not count.
         /// </summary>
         /// <param name="num"></param>
         /// <returns></returns>
-        private static int NumDigits(int num)
+        private static int NumDigits(long num)
         {
             if (num == 0)
             {
@@ -39,7 +40,7 @@ namespace ExportApiSample
         /// <param name="objMgr"></param>
         /// <param name="workspaceId"></param>
         /// <param name="outDirectory">Directory to which we are exporting the files</param>
-        public static void ExportNatives(IObjectManager objMgr, int workspaceId, string outDirectory)
+        public static async void ExportNatives(IObjectManager objMgr, int workspaceId, string outDirectory)
         {
             // check if directory exists and create
             // it if it doesn't
@@ -68,6 +69,61 @@ namespace ExportApiSample
             // specify a batch size
             const int BATCH_SIZE = 200;
 
+            // specify the fields we want returned
+            var fields = new List<FieldRef>
+            {
+                new FieldRef{Name = "Control Number"},
+                new FieldRef{Name = "Extracted Text"},
+                new FieldRef{Name = "Custodian - Single Choice"}
+            };
+
+            // query the workspace for all documents
+            var query = new QueryRequest
+            {
+                ObjectType = new ObjectTypeRef { ArtifactTypeID = TYPE_ID },
+                MaxCharactersForLongTextValues = 25,
+                Fields = fields
+            };
+
+            const int startPage = 0; // index of starting page
+
+            // initialize export so we know how many documents total we 
+            // are exporting
+            ExportInitializationResults initResults =
+                await objMgr.InitializeExportAsync(workspaceId, query, startPage);
+
+            long totalCount = initResults.RecordCount;
+
+            // if total count is evenly divisble by the 
+            // batch size, then we don't have to create any batches
+            // for the leftovers
+            long batchCountMaybe = totalCount / BATCH_SIZE;
+            long batchCountDefinitely = 
+                totalCount % BATCH_SIZE == 0 
+                ? batchCountMaybe 
+                : batchCountMaybe + 1;
+
+            long currBatchCount = 1;
+
+            bool done = false;
+            while (!done)
+            {
+                RelativityObjectSlim[] currentBatch =
+                    await objMgr.RetrieveNextResultsBlockFromExportAsync(workspaceId, initResults.RunID, BATCH_SIZE);
+
+                if (currentBatch != null && currentBatch.Any())
+                {
+                    await Console.Out.WriteLineAsync($"Exporting batch {currBatchCount} of {batchCountDefinitely}.");
+                    foreach (RelativityObjectSlim obj in currentBatch)
+                    {
+                        
+                    }
+                }
+                else
+                {
+                    done = true;
+                }
+            }
         }
     }
 }
