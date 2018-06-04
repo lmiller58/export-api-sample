bea01b6de046b93443abb170ae37ff4101c10e74
a-yu-relativity <ayu@kcura.com>
2018-06-04 13:10:52 -0500

    Add new project using helper

---
+++ UsingHelper/MyExportHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExportApiHelper;
+using Relativity.Services.Objects.DataContracts;
+
+namespace UsingHelper
+{
+    public class MyExportHandler : IExportApiHandler
+    {
+        public bool TheadSafe => throw new NotImplementedException();
+
+        public void After(bool complete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Before(long itemCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Item(RelativityObjectSlim item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UsingHelper/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExportApiHelper;
+using Relativity.Services.Objects.DataContracts;
+
+namespace UsingHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ExportApiHelperConfig
+            {
+                BlockSize = 1000,
+                Fields = new FieldAndType[]
+                {
+                    new FieldAndType
+                    {
+                        Field = new Relativity.Services.Objects.DataContracts.FieldRef
+                        {
+                            Name = "Control Number",
+                        },
+                        Type = FieldTypes.FixedLengthText
+                    },
+
+                    new FieldAndType
+                    {
+                        Field = new FieldRef
+                        {
+                            Name = "Extracted Text"
+                        },
+                        Type = FieldTypes.LongText
+                    }
+                },
+
+                WorkspaceId = 1017273,
+                MaximumInlineTextSize = 1024 * 100,
+                RelativityUrl = new Uri("https://p-dv-vm-ij1ps8v.kcura.corp"),
+                UserName = "relativity.admin@kcura.com",
+                UserPassword = "Test1234!"
+            };
+        }
+    }
+}
